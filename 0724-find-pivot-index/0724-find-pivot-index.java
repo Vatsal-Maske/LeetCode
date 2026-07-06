1dbd3313fe1019f7e4bf0a58df6b92aafd394afa
Vatsal-Maske <maskevatsal87@gmail.com>
2026-07-06 20:30:41 +0530

    Time: 1 ms (98.62%), Space: 47.4 MB (40.3%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+
+        int n = nums.length;
+
+        int leftSum[] = new int[n];
+        int rightSum[] = new int[n];
+
+        // leftSum[i] = sum of elements before i
+        for (int i = 1; i < n; i++) {
+            leftSum[i] = leftSum[i - 1] + nums[i - 1];
+        }
+
+
+        for (int i = n - 2; i >= 0; i--) {
+            rightSum[i] = rightSum[i + 1] + nums[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (leftSum[i] == rightSum[i]) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
