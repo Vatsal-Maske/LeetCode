c00bd8ec2d0fe5db26bb874c00a79ba5e311d0dd
Vatsal-Maske <maskevatsal87@gmail.com>
2026-08-21 00:05:35 +0530

    Time: 2 ms (28.09%), Space: 45.3 MB (81.31%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int compress(char[] chars) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+
+        while(i < chars.length){
+            char currentChar = chars[i];
+            int count = 0;
+
+            while(i < chars.length && currentChar == chars[i]){
+                count++;
+                i++;
+            }
+
+            sb.append(currentChar);
+
+            if(count > 1){
+                sb.append(count);
+            }
+        }
+
+        for(int j = 0; j < sb.length(); j++){
+            chars[j] = sb.charAt(j);
+        }
+
+        return sb.length();
+    }
+}
